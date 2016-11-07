d47441f42b873afec66704cebf636d261c35c1bf
Pridybailo Roman <roman.pridybailo@kruschecompany.com>
2016-11-07 12:46:18 +0200

    Added build script

---
+++ named/new.sh
@@ -0,0 +1,12 @@
+IMAGE_NAME="wacken/named"
+
+case "${1}" in
+	--build | -b )  docker build --rm -t ${IMAGE_NAME} .
+		;;
+	--run | -r ) docker run -d -P -t ${IMAGE_NAME}
+        ;;
+    --help | -h ) printf "usage: ${0} [arg]\n--build,-b\tBuild image\n--run,-r\tRun\n"
+		;;
+	* ) printf "Print ${0} --help for help\n"
+		;;
+esac
