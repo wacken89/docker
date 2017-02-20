5bd45d00411c3e9a11e0f2d242ffbb9d7d9e0580
Pridybailo Roman <roman.pridybailo@kruschecompany.com>
2017-02-20 13:36:36 +0200

    Added telebot

---
+++ telebot/new.sh
@@ -0,0 +1,14 @@
+IMAGE_NAME="wacken/telebot"
+
+case "${1}" in
+	--build | -b )  docker build --rm -t ${IMAGE_NAME} .
+		;;
+	--run | -r ) docker run -d -P -t ${IMAGE_NAME}
+        ;;
+	--push| -p ) docker push ${IMAGE_NAME}
+	;;
+    --help | -h ) printf "usage: ${0} [arg]\n--build,-b\tBuild image\n--run,-r\tRun\n--push,-p\tpush image\n"
+		;;
+	* ) printf "Print ${0} --help for help\n"
+		;;
+esac
